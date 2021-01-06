531dd2323b3c3aee4790c45c5c79ad5837073706
Beginnerat2020 <deity1266@gmail.com>
2021-01-06 22:41:12 +0800

    add register

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 # Create your views here.
 
-posts = [
-    {
-        'author': 'admin',
-        'title': 'Blog Post1',
-        'content': 'First post content',
-        'date_posted': 'Jan 4, 2021'
-    },
-    {
-        'author': 'admin',
-        'title': 'Blog Post2',
-        'content': 'Second post content',
-        'date_posted': 'Jan 4, 2021'
-    }
-
-]
-
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
